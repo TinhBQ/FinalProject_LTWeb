7fe70b295291df7a45a92739674757c9d071ad10
Khowfsix <89312102+Khowfsix@users.noreply.github.com>
2022-11-08 01:35:07 +0700

    init project

---
+++ src/main/java/HomeController/User/ControlerHome.java
@@ -0,0 +1,19 @@
+package HomeController.User;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/template/user/index.html"})
+public class ControlerHome extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/template/user/index.jsp");
+		dispatcher.forward(req, resp);
+	}
+}
